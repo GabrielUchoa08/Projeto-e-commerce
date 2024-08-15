6b69b1903ebbfe19a5a0fe5f5270ff6a654bcc64
Gabriel Uchôa da Escóssia <gabrieluchoa@lern.lan>
2024-08-15 19:21:19 -0300

    primeiro commit

---
+++ src/components/Product.jsx
@@ -0,0 +1,28 @@
+ import React from 'react'
+ import { useState } from 'react';
+ 
+ const Product = ({product, onAddToCart}) => {
+
+  const [quantity, setQuantity] = useState(1);
+
+   return (
+     <div className='product'>
+       <img src={product.image} alt={product.name} width={400} height={300}/>
+       <h3>{product.name}</h3>
+       <p>${product.price}</p>
+       <div className='cart-buttons'>
+        <select onChange={(e) => setQuantity(parseInt(e.target.value))}>
+          {[...Array(10).keys()].map((x)=> (
+            <option key={x+1} value={x+1}>{x+1}</option>
+          ))}
+        </select>
+        <button onClick={()=> onAddToCart(product, quantity)}>
+          Adicionar ao carrinho
+        </button>
+       </div>
+     </div>
+   )
+ }
+ 
+ export default Product
+ 
---
+++ src/components/ThankYouPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+const ThankYouPage = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const items = location.state.cartItems
+
+  const totalPrice= items.reduce((total,item) => total + item.price * item.quantity,0)
+
+  return (
+    <div className='thank-you-page'>
+      <ul>
+        <h1>Pedido Finalizado!</h1>
+        {items.map((item)=> (
+          <li key={item.id}> {item.name} - {item.quantity} x ${item.price} </li>
+        ))}
+      </ul>
+      <p>Total: ${totalPrice.toFixed(2)}</p>
+      <button onClick={() => navigate("/")}>Voltar ao catálogo</button>
+    </div>
+  )
+}
+
+export default ThankYouPage
